84c5ede70a03a025c50fd619cf19ca6751f62b8b
Arthur Camara <arthurcamara@gmail.com>
2016-02-07 11:19:56 -0800

    Update interpolators.js
    Use export default syntax

--- interpolators.js
+++ interpolators.js
@@ -5,26 +5,20 @@
  * @return {Number} y - interpolated value
  */
 //
-var VizabiInterpolators = (function() {
-    return {
-        linear: function(x1, x2, y1, y2, x) {
-          return +y1 + (y2 - y1) * (x - x1) / (x2 - x1);
-        },
-        exp: function(x1, x2, y1, y2, x) {
-          return Math.exp((Math.log(y1) * (x2 - x) - Math.log(y2) * (x1 - x)) / (x2 - x1));
-        },
-        stepBefore: function(x1, x2, y1, y2, x) {
-            return y2;
-        },
-        stepAfter: function(x1, x2, y1, y2, x) {
-            return y1;
-        },
-        stepMiddle: function(x1, x2, y1, y2, x) {
-            return (x < (x1 + x2)/2) ? y1 : y2;
-        }
+export default  {
+    linear: function(x1, x2, y1, y2, x) {
+      return +y1 + (y2 - y1) * (x - x1) / (x2 - x1);
+    },
+    exp: function(x1, x2, y1, y2, x) {
+      return Math.exp((Math.log(y1) * (x2 - x) - Math.log(y2) * (x1 - x)) / (x2 - x1));
+    },
+    stepBefore: function(x1, x2, y1, y2, x) {
+        return y2;
+    },
+    stepAfter: function(x1, x2, y1, y2, x) {
+        return y1;
+    },
+    stepMiddle: function(x1, x2, y1, y2, x) {
+        return (x < (x1 + x2)/2) ? y1 : y2;
     }
-}());
-
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-  module.exports = VizabiInterpolators;
-}
+};
