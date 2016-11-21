69b0410abd899c4596d49e3e67c9d143a71db062
Angie the Fairytale <konzeptmeister@gmail.com>
2016-11-21 19:25:30 +0100

    change `export default` to `module.exports =` in `vizabi-interpolators` (#2)
    * change `export default` to `module.exports =` in `vizabi-interpolators`
    * Update package.json

--- interpolators.js
+++ interpolators.js
@@ -5,7 +5,7 @@
  * @return {Number} y - interpolated value
  */
 //
-export default  {
+module.exports = {
     linear: function(x1, x2, y1, y2, x) {
       return +y1 + (x - x1) / (x2 - x1) * (y2 - y1);
     },
